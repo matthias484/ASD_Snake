9d6f82fa9ea3f3800be8688c1c2f50ea31b42653
MarkusMayer1 <markus.mayer1@stud.fh-campuswien.ac.at>
2023-12-22 13:50:59 +0100

    ingameobjects/Position: Made x and y final

--- src/main/java/at/ac/fhcampuswien/snake/ingameobjects/Position.java
+++ src/main/java/at/ac/fhcampuswien/snake/ingameobjects/Position.java
@@ -2,8 +2,8 @@ package at.ac.fhcampuswien.snake.ingameobjects;
 
 public class Position {
 
-    private int x;
-    private int y;
+    private final int x;
+    private final int y;
 
     public Position(int x, int y) {
         this.x = x;
