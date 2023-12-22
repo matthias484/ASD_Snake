8f39e709c09bc98e527bb03be4d9cfc6ec18412e
MarkusMayer1 <markus.mayer1@stud.fh-campuswien.ac.at>
2023-12-22 11:56:11 +0100

    util/SoundFX: Added JavaDocs to SoundFX class

--- src/main/java/at/ac/fhcampuswien/snake/util/SoundFX.java
+++ src/main/java/at/ac/fhcampuswien/snake/util/SoundFX.java
@@ -7,6 +7,10 @@ import org.slf4j.LoggerFactory;
 import java.net.URISyntaxException;
 import java.util.Objects;
 
+/**
+ * This class encapsulates the sound effects used in the game.
+ * It provides methods to play each sound effect.
+ */
 public class SoundFX {
 
     private static final Logger LOG = LoggerFactory.getLogger(SoundFX.class);
@@ -16,6 +20,9 @@ public class SoundFX {
     private static final String EATING_SOUND = "/sounds/eat.mp3";
     private static final String GAME_OVER_SOUND = "/sounds/game-over.wav";
 
+    /**
+     * Private constructor to hide the implicit public one.
+     */
     private SoundFX() {
 
     }
@@ -36,6 +43,11 @@ public class SoundFX {
         playSoundFx(GAME_OVER_SOUND);
     }
 
+    /**
+     * This method plays the sound effect specified by the resource path.
+     *
+     * @param resourcePath The path to the audio resource.
+     */
     private static void playSoundFx(String resourcePath) {
         try {
             AudioClip audioClip = createAudioClip(resourcePath);
@@ -45,6 +57,13 @@ public class SoundFX {
         }
     }
 
+    /**
+     * Creates an AudioClip from the provided resource path.
+     *
+     * @param resourcePath The path to the audio resource.
+     * @return An AudioClip object.
+     * @throws URISyntaxException If the resource path is not a valid URI.
+     */
     private static AudioClip createAudioClip(String resourcePath) throws URISyntaxException {
         String path = Objects.requireNonNull(SoundFX.class.getResource(resourcePath)).toURI().toString();
         return new AudioClip(path);
