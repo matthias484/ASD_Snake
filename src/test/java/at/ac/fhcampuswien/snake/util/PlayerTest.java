741cae7304a5d40e9786f2b5487b9f4b12a8a1b9
MarkusMayer1 <markus.mayer1@stud.fh-campuswien.ac.at>
2024-01-11 14:48:39 +0100

    Added PlayerTest

---
+++ src/test/java/at/ac/fhcampuswien/snake/util/PlayerTest.java
@@ -0,0 +1,39 @@
+package at.ac.fhcampuswien.snake.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PlayerTest {
+
+    @Test
+    void constructor() {
+        // Arrange
+        String expectedPlayerName = "John Doe";
+        int expectedPlayerScore = 100;
+
+        // Act
+        Player player = new Player(expectedPlayerName, expectedPlayerScore);
+        String actualPlayerName = player.getName();
+        int actualPlayerScore = player.getScore();
+
+        // Assert
+        assertEquals(expectedPlayerName, actualPlayerName);
+        assertEquals(expectedPlayerScore, actualPlayerScore);
+    }
+
+    @Test
+    void toHighScoreString() {
+        // Arrange
+        String expectedHighScoreString = "John Doe%%%100";
+        String playerName = "John Doe";
+        int playerScore = 100;
+
+        // Act
+        Player player = new Player(playerName, playerScore);
+        String actualHighScoreString = player.toHighScoreString();
+
+        // Assert
+        assertEquals(expectedHighScoreString, actualHighScoreString);
+    }
+}
